a7daa1d50e1c57ffe65cb469299ac4981501100b
mengtian <wu750316@163.com>
2018-03-22 19:17:47 +0800

    add 多类逻辑回归

--- utils.py
+++ utils.py
@@ -1,23 +1,110 @@
-import os
-import gzip
-import tarfile
-import struct
+from math import exp
+from mxnet import gluon
+from mxnet import autograd
+from mxnet import nd
+from mxnet import image
+from mxnet.gluon import nn
+import mxnet as mx
 import numpy as np
+from time import time
+import matplotlib.pyplot as plt
 
+class DataLoader(object):
+    """similiar to gluon.data.DataLoader, but might be faster.
 
-def get_data(label_file, data_file):
-    with gzip.open(label_file, 'rb') as fin:
-        struct.unpack(">II", fin.read(8))
-        label = np.fromstring(fin.read(), dtype=np.uint8).astype(np.int32)
-    with gzip.open(data_file, 'rb') as fin:
-        struct.unpack(">IIII", fin.read(16))
-        data = np.fromstring(fin.read(), dtype=np.uint8)
-        data = data.reshape(len(label), 28, 28, 1)
-    return data, label
+    The main difference this data loader tries to read more exmaples each
+    time. But the limits are 1) all examples in dataset have the same shape, 2)
+    data transfomer needs to process multiple examples at each time
+    """
+    def __init__(self, dataset, batch_size, shuffle, transform=None):
+        self.dataset = dataset
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.transform = transform
 
-if __name__ == '__main__':
-    train_data  = get_data('C:\\Users\\mengtian\\.mxnet\\datasets\\fashion-mnist\\train-labels-idx1-ubyte.gz',
-                           'C:\\Users\\mengtian\\.mxnet\\datasets\\fashion-mnist\\train-images-idx3-ubyte.gz')
-    test_data = get_data('C:\\Users\\mengtian\\.mxnet\\datasets\\fashion-mnist\\t10k-labels-idx1-ubyte.gz',
-                           'C:\\Users\\mengtian\\.mxnet\\datasets\\fashion-mnist\\t10k-images-idx3-ubyte.gz')
-    print(train_data)
+    def __iter__(self):
+        data = self.dataset[:]
+        X = data[0]
+        y = nd.array(data[1])
+        n = X.shape[0]
+        if self.shuffle:
+            idx = np.arange(n)
+            np.random.shuffle(idx)
+            X = nd.array(X.asnumpy()[idx])
+            y = nd.array(y.asnumpy()[idx])
+
+        for i in range(n//self.batch_size):
+            if self.transform is not None:
+                yield self.transform(X[i*self.batch_size:(i+1)*self.batch_size], 
+                                     y[i*self.batch_size:(i+1)*self.batch_size])
+            else:
+                yield (X[i*self.batch_size:(i+1)*self.batch_size],
+                       y[i*self.batch_size:(i+1)*self.batch_size])
+
+    def __len__(self):
+        return len(self.dataset)//self.batch_size
+
+def load_data_fashion_mnist(batch_size, resize=None, root="~/.mxnet/datasets/fashion-mnist"):
+    """download the fashion mnist dataest and then load into memory"""
+    def transform_mnist(data, label):
+        # Transform a batch of examples.
+        if resize:
+            n = data.shape[0]
+            new_data = nd.zeros((n, resize, resize, data.shape[3]))
+            for i in range(n):
+                new_data[i] = image.imresize(data[i], resize, resize)
+            data = new_data
+        # change data from batch x height x width x channel to batch x channel x height x width
+        return nd.transpose(data.astype('float32'), (0,3,1,2))/255, label.astype('float32')
+
+    mnist_train = gluon.data.vision.FashionMNIST(root=root, train=True, transform=None)
+    mnist_test = gluon.data.vision.FashionMNIST(root=root, train=False, transform=None)
+    # Transform later to avoid memory explosion. 
+    train_data = DataLoader(mnist_train, batch_size, shuffle=True, transform=transform_mnist)
+    test_data = DataLoader(mnist_test, batch_size, shuffle=False, transform=transform_mnist)
+    return (train_data, test_data)
+
+def SGD(params, lr):
+    for param in params:
+        param[:] = param - lr * param.grad
+
+def accuracy(output, label):
+    return nd.mean(output.argmax(axis=1)==label).asscalar()
+
+def evaluate_accuracy(data_iterator, net, ctx=[mx.cpu()]):
+    if isinstance(ctx, mx.Context):
+        ctx = [ctx]
+    acc = nd.array([0])
+    n = 0.
+    if isinstance(data_iterator, mx.io.MXDataIter):
+        data_iterator.reset()
+    for batch in data_iterator:
+        data, label, batch_size = _get_batch(batch, ctx)
+        for X, y in zip(data, label):
+            acc += nd.sum(net(X).argmax(axis=1)==y).copyto(mx.cpu())
+            n += y.size
+        acc.wait_to_read() # don't push too many operators into backend
+    return acc.asscalar() / n
+
+def show_images(imgs, nrows, ncols, figsize=None):
+    """plot a list of images"""
+    if not figsize:
+        figsize = (ncols, nrows)
+    _, figs = plt.subplots(nrows, ncols, figsize=figsize)
+    for i in range(nrows):
+        for j in range(ncols):
+            figs[i][j].imshow(imgs[i*ncols+j].asnumpy())
+            figs[i][j].axes.get_xaxis().set_visible(False)
+            figs[i][j].axes.get_yaxis().set_visible(False)
+    plt.show()
+
+def _get_batch(batch, ctx):
+    """return data and label on ctx"""
+    if isinstance(batch, mx.io.DataBatch):
+        data = batch.data[0]
+        label = batch.label[0]
+    else:
+        data, label = batch
+    return (gluon.utils.split_and_load(data, ctx),
+            gluon.utils.split_and_load(label, ctx),
+            data.shape[0])
